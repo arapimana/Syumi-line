3cbcaca026fe3b111d3d947adf30c964fc9dea1c
arapimana <129587248+arapimana@users.noreply.github.com>
2026-04-20 14:19:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function checkAnswer() {
-
     const answerInput = document.getElementById('answerInput').value.trim().toLowerCase();
     const result = document.getElementById('result');
 
-    if (answerInput === "showdown") {
-        result.innerHTML = '<a href="https://arapimana.github.io/Syumi-line/" target="_blank" style="color: #00ff00; font-weight: bold; font-size: 20px;">ライセンス資格確認</a>';
+    const hashedInput = CryptoJS.MD5(answerInput).toString();
+
+    const targetHash1 = "115c87258963fd94d34e6a96a9c4d27d";
+
+    if (hashedInput === targetHash1) {
+
+        result.innerHTML = '<a href="https://arapimana.github.io/Syumi-line/" target="_blank" style="color: #00ff00; font-weight: bold; font-size: 20px; text-decoration: underline;">ライセンス資格確認</a>';
     } else {
         result.textContent = "ライセンス資格なし";
+        result.style.color = "red";
     }
 }
