9ecce23cb61207de99117009ca824d40fd8a409e
arapimana <129587248+arapimana@users.noreply.github.com>
2026-04-20 13:46:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 function checkAnswer() {
-    const answerInput = document.getElementById('answerInput').value;
+
+    const answerInput = document.getElementById('answerInput').value.trim().toLowerCase();
     const result = document.getElementById('result');
 
-
-    
     const hashedInput = CryptoJS.MD5(answerInput).toString();
 
 
     const targetHash1 = "772f44c4ca008985390979379637890b";
-    
-
-    const targetHash2 = "87b3a4a16223592186717a6f23023a1d"; 
 
     if (hashedInput === targetHash1) {
         result.innerHTML = '<a href="https://arapimana.github.io/ohayou/" target="_blank">ライセンス資格確認</a>';
-    } else if (hashedInput === targetHash2) {
-        result.textContent = "???????";
     } else {
         result.textContent = "ライセンス資格なし";
     }
